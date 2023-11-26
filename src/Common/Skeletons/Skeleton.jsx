1f68e81caec8c597dbaff448c67137872e636cdc
ihtishamhussain <ihtishamshami72@gmail.com>
2023-11-26 09:43:11 -0800

    second commit

--- src/Common/Skeletons/Skeleton.jsx
+++ src/Common/Skeletons/Skeleton.jsx
@@ -1,7 +1,6 @@
 import "./Skeleton.css";
 
 const Skeleton = ({ classes }) => {
-    console.log(classes,'classes');
   const classNames = `skeleton ${classes} animate-pulse`;
   return <div className={classNames}></div>;
 };
