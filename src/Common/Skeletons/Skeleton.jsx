0815c000cf79695c599b0863565fe73bba5b95ad
ihtishamhussain <ihtishamshami72@gmail.com>
2023-11-26 09:41:36 -0800

    strzplay commit

--- src/Common/Skeletons/Skeleton.jsx
+++ src/Common/Skeletons/Skeleton.jsx
@@ -1,8 +1,8 @@
 import "./Skeleton.css";
 
 const Skeleton = ({ classes }) => {
+    console.log(classes,'classes');
   const classNames = `skeleton ${classes} animate-pulse`;
-
   return <div className={classNames}></div>;
 };
 export default Skeleton;
